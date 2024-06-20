1f96c96c181a4f21612a784415bf5c496264ba04
Андрей <jokerjam@yandex.ru>
2024-06-20 17:36:50 +0300

    progress

--- udmy-app/src/App.js
+++ udmy-app/src/App.js
@@ -1,11 +1,35 @@
-
+import Costs from "./components/Costs";
 
 function App() {
-  return (
-    <div>
-      <h1>Start React lerning.</h1>
-    </div>
-  );
+    const costs = [
+        {
+            date: new Date(2022, 11, 10),
+            description: "Холодильник",
+            amount: 999.99,
+        },
+        {
+            date: new Date(2023, 8, 16),
+            description: "Телевизор",
+            amount: 999.99,
+        },
+        {
+            date: new Date(2021, 10, 8),
+            description: "Пылесос",
+            amount: 999.99,
+        },
+        {
+            date: new Date(2022, 9, 15),
+            description: "Ноутбук",
+            amount: 999.99,
+        },
+    ]
+
+    return (
+        <div>
+            <h1>Start React lerning.</h1>
+            <Costs costs={costs} />
+        </div>
+    );
 }
 
 export default App;
